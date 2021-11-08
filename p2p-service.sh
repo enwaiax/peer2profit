c11d75a3077677a22c0a71469b6d1550e04a455f
LuckyHunter <chasing66@live.com>
2021-11-08 22:39:46 +0800

    Update p2p-service.sh script

--- p2p-service.sh
+++ p2p-service.sh
@@ -16,9 +16,9 @@ function install_dependencies() {
 
 function install_p2p_service() {
     echo "Installing p2p-service..."
-    wget https://updates.peer2profit.com/p2pclient_0.56_amd64.deb
-    dpkg -i ./p2pclient_0.54_amd64.deb
-    rm -f ./p2pclient_0.54_amd64.deb
+    wget https://updates.peer2profit.com/p2pclient_0.56_amd64.deb -O p2p-service.deb
+    dpkg -i p2p-service.deb
+    rm -f p2p-service.deb
 }
 
 function start_p2p_service() {
